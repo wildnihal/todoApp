d93a2626da1d3ebe26142f5810de02434fdaf0a2
Nihaal Dhakal <nihaldhakal41@gmail.com>
2019-07-16 10:08:47 +0545

    Created new,create, show

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -2,4 +2,34 @@ class TodoListsController < ApplicationController
   def index
     @list = TodoList.all
   end
+
+  def new
+    @list = TodoList.new
+  end
+
+  def create
+    @list = TodoList.new(todo_list_params)
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to  @list, notice: 'Todo list was successfully created.' }
+        format.json { render :show, status: :created, location: @list }
+      else
+        format.html { render :new }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  
+
+  def show
+    @list = TodoList.last
+  end
+
+  private
+
+  def todo_list_params
+    params.require(:todo_list).permit(:title, :description)
+  end
+
 end
