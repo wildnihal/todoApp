38a91217881002257255437a60d5f6d1c3d49f74
Nihaal Dhakal <nihaldhakal41@gmail.com>
2019-09-17 14:55:02 +0545

    Sign out issuse has been fixed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
    # get '/todo_lists' => 'todo_lists#index'
   resources :todo_lists, only: [:index, :new, :create, :edit, :update, :show]
   root "todo_lists#index"
- 
+  devise_for :users
+  resources :users do
+    resources :todo_lists
+  end
 end
